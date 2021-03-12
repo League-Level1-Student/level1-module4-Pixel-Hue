d2894b963eda81954d1224a66e79e3e51363125c
student <student@ip-172-31-62-226.us-west-2.compute.internal>
2021-03-11 16:54:19 -0800

    safefefeffdsfsafeaefadsdadsadfdsafdsafadsfaefasef

---
+++ src/_01_nasty_surprise/NastySuprise.java
@@ -0,0 +1,21 @@
+package _01_nasty_surprise;
+
+import java.awt.Frame;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class NastySuprise {
+public static void main(String[] args) {
+	JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JButton button = new JButton();
+JButton button2 = new JButton();
+JLabel trick = new JLabel();
+JLabel Treat = new JLabel();
+trick.setText("Trick");
+Treat.setText("Treat");
+}
+}
